3d5bf984e0b0470604b5610e0d216cdce39da3bd
tuncerrstm <tuncerrstm@gmail.com>
2022-05-22 19:26:49 +0300

    Chat Room Messages CRUD : Completed

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -89,13 +89,17 @@ def home(request):
 def room(request, pk):
     room = Room.objects.get(id=pk)
     room_messages = room.message_set.all().order_by('-created')
-
-    if request.method == 'post':
+    paticipants = room.paticipants.all()
+    if request.method == 'POST':
         message = Message.objects.create(
-            
+            user=request.user,
+            room=room,
+            body=request.POST.get('body')
         )
+        room.paticipants.add(request.user)
+        return redirect('room', pk=room.id)
 
-    context = {'room': room, 'room_messages': room_messages}
+    context = {'room': room, 'room_messages': room_messages, 'paticipants': paticipants}
     return render(request, 'base/room.html', context)
 
 @login_required(login_url='login')
@@ -141,3 +145,17 @@ def deleteRoom(request, pk):
         room.delete()
         return redirect('home')
     return render(request, 'base/delete.html', {'obj': room})
+
+
+
+@login_required(login_url='login')
+def deleteMessage(request, pk):
+    message = Message.objects.get(id=pk)
+
+    if request.user != message.user:
+        return HttpResponse('You are not allowed here!!')
+
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj': message})
